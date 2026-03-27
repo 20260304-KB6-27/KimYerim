2908fcd903814f133a3fb84e3d746d43da3162c0
Kimyerim <yeye092302@gmail.com>
2026-03-27 16:47:14 +0900

    feat: Composition 실습 파일 업로드

---
+++ 03_Vue/02_advanced/08_Composition/todolist/src/App.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="container">
+        <div class="card card-body bg-light">
+            <div class="title">:: Todolist App</div>
+        </div>
+        <div class="card card-default card-borderless">
+            <div class="card-body">
+                <InputTodo @add-todo="addTodo"></InputTodo>
+                <TodoList
+                    :todoList="todoList"
+                    @delete-todo="deleteTodo"
+                    @toggle-completed="toggleCompleted"
+                ></TodoList>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import InputTodo from './components/InputTodo.vue';
+import TodoList from './components/TodoList.vue';
+import { ref } from 'vue';
+
+const ts = new Date().getTime();
+
+const todoList = ref([
+    { id: ts, todo: '자전거 타기', completed: false },
+    { id: ts + 1, todo: '딸과 공원 산책', completed: true },
+    {
+        id: ts + 2,
+        todo: '일요일 애견 카페',
+        completed: false,
+    },
+    { id: ts + 3, todo: 'Vue 원고 집필', completed: false },
+]);
+
+const addTodo = (todo) => {
+    if (todo.length >= 2) {
+        todoList.value.push({
+            id: new Date().getTime(),
+            todo: todo,
+            completed: false,
+        });
+    }
+};
+
+const deleteTodo = (id) => {
+    let index = todoList.value.findIndex((item) => id === item.id);
+    todoList.value.splice(index, 1);
+};
+
+const toggleCompleted = (id) => {
+    let index = todoList.value.findIndex((item) => id === item.id);
+    todoList.value[index].completed = !todoList.value[index].completed;
+};
+</script>
+
+<style scoped></style>
