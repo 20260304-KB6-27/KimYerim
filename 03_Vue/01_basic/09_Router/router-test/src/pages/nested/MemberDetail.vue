88089f8cb1f478734cf51b678f7aa054615a6066
Kimyerim <yeye092302@gmail.com>
2026-04-02 15:38:38 +0900

    feat: Axios 실습 파일 업로드

---
+++ 03_Vue/01_basic/09_Router/router-test/src/pages/nested/MemberDetail.vue
@@ -0,0 +1,22 @@
+<template>
+    <div v-if="member">
+        <img :src="member.photo" alt="프로필 사진" />
+        <h2>{{ member.name }}</h2>
+        <h2>{{ member.role }}</h2>
+        <h2>{{ member.desc }}</h2>
+    </div>
+</template>
+
+<script setup>
+import { useRoute } from 'vue-router';
+import { computed } from 'vue';
+import members from '@/members.json';
+
+const route = useRoute();
+
+const member = computed(() =>
+    members.find((m) => m.id === Number(route.params.id)),
+);
+</script>
+
+<style scoped></style>
